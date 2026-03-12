db965a2dfeabf01bfe734dc0417e5cb6155b3644
luiza <duda2005056@gmail.com>
2026-03-11 20:53:32 -0400

    Parte-2-atv

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
-import Footer from './components/footer'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import BemVindo from './components/BemVindo'
 
 function App() {
   return (
     <div>
-      <h1>My Daily Habits</h1>
-      <p>Gerencie seus hábitos diários de forma simples e visual</p>
+      <Header 
+      titulo="My Daily Habits" 
+      descrição="Gerencie seus hábitos diários de forma simples e visual" />
+
+      <BemVindo
+        nomeUsuario= "Luiza"
+        totalHabitos= {564}
+        />
+
       <Footer />
     </div>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header ({ titulo, descrição }) {
+    return (
+        <div>
+            <h1>{titulo}</h1>
+            <p>{descrição}</p>
+        </div>
+    )
+}
+
+export default Header

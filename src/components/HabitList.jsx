ace719a100f937acf398df083d9b2e43589f3983
luiza <duda2005056@gmail.com>
2026-03-16 20:57:16 -0400

    terminar atv 3

--- src/components/HabitList.jsx
+++ src/components/HabitList.jsx
@@ -1,25 +1,97 @@
-import HabitCard from "./HabitCard";
+import { useState, useEffect } from 'react';
+import HabitCard from './HabitCard';
 
-function HabitList({ habits }) {
-    if (!habits) return null
+function HabitList() {
+    const [habits, setHabits] = useState
 
-    if (habits.length === 0) {
-        return <p>Nenhum hábito cadastrado ainda. Que tal começar?</p>
-    }
+    useEffect(() => {
+  document.title = 'My Daily Habits — '${habits.length}' hábito'(s)
+  console.log('useEffect rodou')
+})
 
+    const removerHabit = (id) => {
+        setHabits(habits.filter(habit => habit.id !== id))
+}
+       
     return (
         <ul>
-            {habits.map( ( habits) => (
+            {habits.length === 0 && <p>Nenhum cadastrado ainda. Que tal começar?</p>}
+
+            {habits.map((habit) => (
                 <HabitCard
-                    key={habits.id}
-                    titulo={habits.titulo}
-                    meta={habits.meta}
-                    ativo={habits.ativo}
-                    diasFeitos={habits.diasFeitos}
-                />
+                key={habit.id}
+                nome={habit.nome}
+                descrição={habit.descrição}
+                meta={habit.meta}
+                ativo={habit.ativo}
+                diasFeitos={habit.diasFeitos}
+                onRemover={ () => removerHabit (habit.id) }
+            />
             ))}
         </ul>
     )
 }
 
+const [novoNome, setNovoNome] = useState('')
+const [novaDescricao, setNovaDescricao] = useState('')
+const [novaCategoria, setNovaCategoria] = useState('')
+
+const adicionarHabit = (event) => {
+    event.preventDefault()
+
+    if (!novoNome.trim()) {
+        alert ('Informe um nome para o hábito.')
+        return
+    }
+
+    const novoHabit = {
+        id: Date.now(),
+        nome: novoNome,
+        descrição: novaDescricao,
+        meta: 7,
+        ativo: true,
+        diasFeitos: 0,
+        categoria: novaCategoria || 'Geral',
+    }
+
+    setHabits([...habits, novoHabit])
+    
+
+    setNovoNome('')
+    setNovaDescricao('')
+    setNovaCategoria('')
+
+return (
+    <section>
+    <form onSubmit={adicionarHabit} className="habit-form">
+            <div>
+                <label>
+                    Nome do hábito *
+                    <input 
+                    type="text"
+                    value={novoNome}
+                    onchange={ (e) => setNovoNome(e.target.value)}
+                    />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Descrição
+                    <input
+                    type="text"
+                    value={novaDescricao}
+                    onChange={ (e) => setNovaDescricao(e.target.value)}
+                    />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Categoria
+                    
+                </label>
+            </div>
+        </form>
+    </section>
+)
+}
 export default HabitList
